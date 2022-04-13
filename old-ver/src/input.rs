09de140a6fd4faf31de99e4df19a96d08b7c8d95
Parker Lamb <andromeda@outlook.ie>
2022-04-12 21:30:33 -0600

    Added older version

---
+++ old-ver/src/input.rs
@@ -0,0 +1,18 @@
+// Description: functions to deal with file inputs and CLI arguments.
+extern crate clap;
+
+use clap::{App, Arg, ArgMatches};
+
+// Get all CLI arguments, if they exist. 
+pub fn get_args() -> ArgMatches<'static> {
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+            .version(env!("CARGO_PKG_VERSION"))
+            .author(env!("CARGO_PKG_AUTHORS"))
+            .about(env!("CARGO_PKG_DESCRIPTION"))
+            .arg(Arg::with_name("INPUT")
+                .help("Input image")
+                .required(false)
+                .index(1))
+            .get_matches();
+    matches
+}
