995254cb8bff131a9239e9772cf0ad3c46622c13
Parker Lamb <andromeda@outlook.ie>
2022-04-15 20:58:48 -0600

    Added file recognition and directory file processing

--- src/functions.rs
+++ src/functions.rs
@@ -27,19 +27,55 @@ pub fn get_dir(arguments: &ArgMatches) -> Option<PathBuf> {
             let path_dir = PathBuf::from(path_canon.to_str().unwrap().strip_suffix(fname).unwrap());
             if path_dir.is_dir() {
                 return Some(path_dir);
+            } else {
+                return None
             }
+        } else {
+            return None
         }
     } else {
         return None
     }
 
-
-    // Otherwise,
-
-    Some(path)
-
 }
 
-// pub fn get_files
+// Get a list of all files within a given directory
+pub fn get_files(dir: &PathBuf) -> Option<Vec<PathBuf>> {
+    // Only return top-level files
+    let mut files = Vec::<PathBuf>::new();
+    for file in dir.read_dir().expect("Unable to read directory") {
+        if let Ok(file) = file {
+            files.push(file.path());
+        }
+    }
+    if !files.is_empty() {
+        Some(files)
+    } else {
+        None
+    }
+}
 
-// pub fn get_index
+
+// Return index of file within list of files, if specified
+pub fn get_index(arguments: &ArgMatches, files: &Vec<PathBuf>) -> Option<usize> {
+    // Get the path string from arguments
+    let arg = arguments.value_of("INPUT");
+
+    // Create PathBuf if path string exists
+    let path;
+    match arg {
+        Some(p) => path = PathBuf::from(p),
+        None => return None
+    }
+
+    if path.exists() {
+        if path.is_file() {
+            let index = files.iter().position(|k| k == &path.canonicalize().unwrap());
+            return index;
+        } else {
+            return None;
+        }
+    } else {
+        return None;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,35 @@ mod input;
 mod gui;
 
 fn main() {
-    // 0. Base parameters
-    let supported_formats = vec!["jpg","JPG","jpeg","JPEG","png","PNG"];
-
     // 1. Get application CLI arguments
     let arguments = input::get_args();
 
-    // 2. Create application instance
+    // 2. Determine if we should open a directory, a file, or nothing at all
     let dir = functions::get_dir(&arguments);
+
+    // 3. Scan files in directory
+    let files;
+    let index;
+    match dir {
+        Some(d) => {
+            files = functions::get_files(&d);
+            match files {
+                Some(ref f) => index = functions::get_index(&arguments, &f),
+                None => index = None
+            }
+        },
+        None => {files = None; index = None;}
+    }
+
+    match files {
+        Some(f) => {
+            match index {
+                Some(i) => println!("{:?}",f[i]),
+                None => println!("Directory specified")
+            }
+        }
+        None => println!("Nothing specified")
+    }
     
     // 3. Open GTK window, and hand off management to it
     // ** Open image data in window if provided
